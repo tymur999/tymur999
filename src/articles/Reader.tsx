501c195d79cbaf155a63d285cff2bc357de3211d
tymur999 <tymur.arsent@gmail.com>
2026-04-08 16:26:37 -0500

    add close button reader

--- src/articles/Reader.tsx
+++ src/articles/Reader.tsx
@@ -4,6 +4,8 @@ import {useEffect} from "react";
 import {AnimateFade, AnimateTemplate} from "../animations";
 import "./reader.sass";
 import {ExternalLink} from "../router/Link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faClose} from "@fortawesome/free-solid-svg-icons/faClose";
 
 const READER : AnimateTemplate = AnimateFade();
 
@@ -21,7 +23,7 @@ export function Reader() {
 
   function handleClose() {
     Promise.all([
-        animate(".reader", READER.initial),
+        animate(".reader", READER.initial)
       ]
     ).then(() => setPost(undefined));
   }
@@ -34,6 +36,9 @@ export function Reader() {
       onClick={handleClose}
       className="reader link">
         <article onClick={e => e.stopPropagation()}>
+          <button onClick={handleClose} className="link reader-close">
+            <FontAwesomeIcon icon={faClose} />
+          </button>
           <post.article components={{
            'a': ExternalLink
           }}/>
